73611a8c072aea278a98b7171322457e2a126a81
Florent Philippe <florent.philippe17@gmail.com>
2017-03-10 17:10:15 +0100

    Creating a completly new NewDrug activity with its XML file

--- app/src/main/java/fr/florentphilippe/lay/NewDrug.java
+++ app/src/main/java/fr/florentphilippe/lay/NewDrug.java
@@ -3,11 +3,11 @@ package fr.florentphilippe.lay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NewDrug extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_drug);
     }
 }
