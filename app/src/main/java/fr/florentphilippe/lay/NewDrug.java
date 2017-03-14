3898f154dcf15f5e0b03c2c4d8889e1337281ed7
Florent Philippe <florent.philippe17@gmail.com>
2017-03-14 21:41:46 +0100

    Designing the new activity_new_drug.xml, now based on a CoordinateLayout

--- app/src/main/java/fr/florentphilippe/lay/NewDrug.java
+++ app/src/main/java/fr/florentphilippe/lay/NewDrug.java
@@ -2,13 +2,19 @@ package fr.florentphilippe.lay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class NewDrug extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //removing the title bar feature
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_new_drug);
+
+
     }
 
 }
