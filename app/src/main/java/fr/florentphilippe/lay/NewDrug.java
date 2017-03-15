a65f6812b3b6612fbc8193396f74c6dd7b8364a9
Florent Philippe <florent.philippe17@gmail.com>
2017-03-15 17:55:57 +0100

    Working on the activity_new_drug theme

--- app/src/main/java/fr/florentphilippe/lay/NewDrug.java
+++ app/src/main/java/fr/florentphilippe/lay/NewDrug.java
@@ -1,16 +1,19 @@
 package fr.florentphilippe.lay;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 
 public class NewDrug extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //removing the title bar feature
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //make the status bar transparent
+        View decorView = getWindow().getDecorView();
+
+
 
         setContentView(R.layout.activity_new_drug);
 
