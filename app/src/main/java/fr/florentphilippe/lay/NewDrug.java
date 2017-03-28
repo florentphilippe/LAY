f3ce944a8acccfef1c83375ef697b8e54e59500d
Florent Philippe <florent.philippe17@gmail.com>
2017-03-28 21:21:43 +0200

    Adding buttons for selecting date in activity ew drug. Setting them to the current date

--- app/src/main/java/fr/florentphilippe/lay/NewDrug.java
+++ app/src/main/java/fr/florentphilippe/lay/NewDrug.java
@@ -1,9 +1,16 @@
 package fr.florentphilippe.lay;
 
-import android.support.v4.view.ViewPager;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class NewDrug extends AppCompatActivity {
 
@@ -11,6 +18,27 @@ public class NewDrug extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_drug);
+
+        //Importing EditText from UI
+        Button buttonStartDate = (Button) findViewById(R.id.button_start_date);
+        Button buttonEndDate = (Button) findViewById(R.id.button_end_date);
+
+        //Initialise the current date
+        Calendar currentCalendar = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String string = dateFormat.format(currentCalendar.getTime());
+
+        //Setting current date to the button's text
+        buttonStartDate.setText(string);
+        buttonEndDate.setText(string);
+
+
+
+
+
+
+
+
 
 
     }
