6aa602501f8e2fc1109a6fdc4a00b2c6a0af78aa
Florent Philippe <florent.philippe17@gmail.com>
2017-04-10 13:51:48 +0200

    Adding date parameters into the Drug class

--- app/src/main/java/fr/florentphilippe/lay/classes/Drug.java
+++ app/src/main/java/fr/florentphilippe/lay/classes/Drug.java
@@ -1,11 +1,16 @@
 package fr.florentphilippe.lay.classes;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
-//This class represent the "Drug" Object and all its parameters
+/*
+This class represent the "Drug" Object and all its parameters
+ */
 public class Drug implements Serializable{
 
     private String drugName;
     private String laboratoryName;
+    private Calendar startDate;
+    private Calendar endDate;
 
 }
