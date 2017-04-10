10eddb12b12ab155fe8d36ee9698b9410170ed8b
Florent Philippe <florent.philippe17@gmail.com>
2017-04-10 19:48:03 +0200

    Updating "Tools" class

--- app/src/main/java/fr/florentphilippe/lay/Tools.java
+++ app/src/main/java/fr/florentphilippe/lay/Tools.java
@@ -11,27 +11,20 @@ public class Tools {
     //Text button converter from string to calendar
     public static Calendar stringConverterToDate (String buttonText){
 
-        //Dates values
-        int year;
-        int month;
-        int day;
 
         Calendar calendar = Calendar.getInstance();
 
-        //processing the buttonText String
+        //processing the buttonText String using a StringBuilder
         calendar.set(Calendar.DAY_OF_MONTH,Integer.parseInt(new StringBuilder().append(buttonText.charAt(0))
                                                                                  .append(buttonText.charAt(1))
                                                                                  .toString()));
-        month = Integer.parseInt(new StringBuilder(buttonText.charAt(3)).append(buttonText.charAt(4)).toString());
-        year = Integer.parseInt(new StringBuilder(buttonText.charAt(6)).append(buttonText.charAt(7))
+        calendar.set(Calendar.MONTH, Integer.parseInt(new StringBuilder().append(buttonText.charAt(3))
+                                                                        .append(buttonText.charAt(4))
+                                                                        .toString()));
+        calendar.set(Calendar.YEAR, Integer.parseInt(new StringBuilder(buttonText.charAt(6)).append(buttonText.charAt(7))
                                                                         .append(buttonText.charAt(8))
                                                                         .append(buttonText.charAt(9))
-                                                                        .toString());
-
-        //Setting the calendar's values
-        calendar.set(Calendar.YEAR,year);
-        calendar.set(Calendar.MONTH,month);
-        //calendar.set(Calendar.DAY_OF_MONTH,day);
+                                                                        .toString()));
 
         return calendar;
     }
