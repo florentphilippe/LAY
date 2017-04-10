e978db7e6f92c80bc89fb7a269da929ba33ef467
Florent Philippe <florent.philippe17@gmail.com>
2017-04-10 19:53:55 +0200

    Updating all Drug's class parameters

--- app/src/main/java/fr/florentphilippe/lay/classes/Drug.java
+++ app/src/main/java/fr/florentphilippe/lay/classes/Drug.java
@@ -8,9 +8,18 @@ This class represent the "Drug" Object and all its parameters
  */
 public class Drug implements Serializable{
 
+    //Main titles and period
     private String drugName;
     private String laboratoryName;
     private Calendar startDate;
     private Calendar endDate;
+
+    //Frequency
+    private Integer timesInTheFrequency;
+    private String frequency;
+
+    //Time in the frequency
+    private String time;
+    private String relativeTime;
 
 }
